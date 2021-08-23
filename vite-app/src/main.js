5cc117731e63641906867edac1ced2228a390500
田月荣 <tianyuerong@vchangyi.com>
2021-08-23 18:10:03 +0800

    fix：增加vuex4.0

--- vite-app/src/main.js
+++ vite-app/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from "./router.js"
+import {store} from "./store/store.js"
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 const app = createApp(App)
 app.config.productionTip = false;
 
-app.use(Antd).use(router)
+app.use(Antd)
+.use(router)
+.use(store)
 .mount('#app')
--- vite-app/src/views/about/about.vue
+++ vite-app/src/views/about/about.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="about">
   关于页面
+  <p>数值：{{this.$store.state.count}}</p>
+  <button @click="add">增加</button>
 </div>
 </template>
 <script>
@@ -8,5 +10,10 @@ export default {
   setup() {
     
   },
+  methods:{
+    add(){
+      this.$store.commit('increment')
+    }
+  }
 }
 </script>
