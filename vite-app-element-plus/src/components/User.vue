24e36f9792f6bf88bb9d4936127d856b6eeee194
田月荣 <tianyuerong@vchangyi.com>
2021-08-24 10:48:50 +0800

    setup中的provide与inject

--- vite-app-element-plus/src/components/User.vue
+++ vite-app-element-plus/src/components/User.vue
@@ -2,12 +2,13 @@
 <div class="about">
   用户名：{{name}}
   分数：{{scores}}
+  <div ref='root'>this is root element</div>
 </div>
 </template>
 <script>
 import userScore from '@/composables/useUserScore'
 import searchScore from '@/composables/useSearchScore'
-import { toRefs } from 'vue'
+import { toRefs,toRef,ref,onMounted } from 'vue'
 export default {
   name:'user',
   props:{
@@ -20,12 +21,17 @@ export default {
     return{
     }
   },
-  setup(props) {
-    const {name} = toRefs(props)
+  setup(props,{attrs,slots,emit}) {
+    const root = ref(null)
+    onMounted(()=>{
+      console.log(11,root.value)
+    })
+    const name = toRef(props,'name')
+    console.log(name.value)
     const {scores,fetchScores} = userScore(name);
     const {searchQuery,scoresSearchQuery} = searchScore(scores)
-
     return {
+      root,
       scores: scoresSearchQuery,
       fetchScores,
       searchQuery
--- vite-app-element-plus/src/views/about/about.vue
+++ vite-app-element-plus/src/views/about/about.vue
@@ -3,19 +3,28 @@
   关于页面
   <p>数值：{{this.$store.state.count}}</p>
   <button @click="add">增加</button>
-  <user name="zmm"></user>
+  <user :name="userName"></user>
+  <button @click="change">修改名称</button>
 </div>
 </template>
 <script>
 import User from "@/components/User.vue"
 export default {
   components:{User},
+  data(){
+    return{
+      userName:'zmm'
+    }
+  },
   setup() {
     
   },
   methods:{
     add(){
       this.$store.commit('increment')
+    },
+    change(){
+      this.userName = 'tyr'
     }
   }
 }
