00738a16a665891a0abd5842ae578b2792b56237
田月荣 <tianyuerong@vchangyi.com>
2021-09-06 23:39:32 +0800

    feat:高德地图位置标注组件

---
+++ vite-kit/vite-kit/src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Map/>
+</div>
+  
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Map from "./components/Map.vue";
+export default({
+	components:{HelloWorld,Map},
+	setup() {
+	},
+});
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite-kit/vite-kit/src/components/Map.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="map-container"></div>
+</template>
+<script>
+import {onMounted,toRefs} from "vue";
+import AMapLoader from "@amap/amap-jsapi-loader";
+
+export default({
+	props:{
+		position:{
+			type:Array
+		}
+	},
+	setup(props) {
+		const {position} = toRefs(props);
+		onMounted(()=>{
+			AMapLoader.load({
+				"key": "9866f8b589d15c14beb78cd54dad0c37",              // 申请好的Web端开发者Key，首次调用 load 时必填
+				"version": "1.4.15",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+				"plugins": [],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+				"AMapUI": {             // 是否加载 AMapUI，缺省不加载
+					"version": "1.1",   // AMapUI 缺省 1.1
+					"plugins":[],       // 需要加载的 AMapUI ui插件
+				},
+				"Loca":{                // 是否加载 Loca， 缺省不加载
+					"version": "1.3.2"  // Loca 版本，缺省 1.3.2
+				},
+			}).then((AMap)=>{
+				let map = new AMap.Map("map-container",{
+					center:[108.93425, 34.23053],
+					// viewMode:"3D",//使用3D视图
+					zoom: 13,//设置地图级别范围
+				});
+				var marker = new AMap.Marker({
+					position:[108.93425, 34.23053],//位置
+				});
+				map.add(marker);//添加到地图
+				//实时路况图层
+				// var trafficLayer = new AMap.TileLayer.Traffic({
+				// 	zIndex: 10
+				// });
+				// map.add(trafficLayer);//添加图层到地图
+			}).catch(e => {
+				console.log(e);
+			});
+		});
+	},
+});
+</script>
+<style lang="scss" scoped>
+#map-container{
+  width: 400px;
+  height: 300px;
+}
+</style>
