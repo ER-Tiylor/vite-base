d0ee0d39cc552fdc3b8019e53b95e60316276b59
田月荣 <tianyuerong@vchangyi.com>
2021-11-15 16:34:01 +0800

    fix:..

---
+++ material-widget/docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'material-widget',
+  description: 'Just playing around.',
+  lang:'zh-CN',
+  themeConfig:{
+    nav:[
+      {text:'首页',link:'/'},
+      {text:'关于',link:'/guide'}
+    ],
+    sidebar:[
+      {text:"门户站点",link:'/pc/'},
+      {text:"管理后台",link:'/admin/'},
+      {text:"M端多页",link:'/mobile/'}
+    ]
+  }
+}
